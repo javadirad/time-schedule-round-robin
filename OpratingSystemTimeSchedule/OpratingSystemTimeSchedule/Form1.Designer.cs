74a8f521ed0bd55481cf959d0156cf8718c7995a
javadirad <mohammad.javadirad@gmail.com>
2019-01-26 07:46:16 +0330

    Round Robin Time Schedule
    First Version

---
+++ OpratingSystemTimeSchedule/OpratingSystemTimeSchedule/Form1.Designer.cs
@@ -0,0 +1,358 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace OpratingSystemTimeSchedule
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textAvgWaitingTime = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.labelSliceTime = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textAvgWaitingTime);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.listBox5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.listBox4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.listBox3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.listBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Location = new System.Drawing.Point(0, 304);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1159, 257);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Log";
+            // 
+            // textAvgWaitingTime
+            // 
+            this.textAvgWaitingTime.Location = new System.Drawing.Point(951, 223);
+            this.textAvgWaitingTime.Name = "textAvgWaitingTime";
+            this.textAvgWaitingTime.Size = new System.Drawing.Size(152, 20);
+            this.textAvgWaitingTime.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(910, 224);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "AVG=";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(974, 17);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "WaitingTime";
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(887, 33);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(247, 173);
+            this.listBox5.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(126, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Ready to do list";
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(17, 33);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(278, 212);
+            this.listBox4.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(403, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Queue status";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(301, 33);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(278, 212);
+            this.listBox3.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(627, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ongoing work";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(585, 33);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(163, 212);
+            this.listBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(777, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "The work done";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(754, 33);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(127, 212);
+            this.listBox1.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.labelSliceTime);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1159, 78);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "options";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(297, 43);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "delay:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(338, 40);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(85, 20);
+            this.textBox3.TabIndex = 7;
+            this.textBox3.Text = "100";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(129, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 43);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Context Switch Time:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(293, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(140, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Automatic Add New Job";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // labelSliceTime
+            // 
+            this.labelSliceTime.AutoSize = true;
+            this.labelSliceTime.Location = new System.Drawing.Point(67, 20);
+            this.labelSliceTime.Name = "labelSliceTime";
+            this.labelSliceTime.Size = new System.Drawing.Size(56, 13);
+            this.labelSliceTime.TabIndex = 3;
+            this.labelSliceTime.Text = "SliceTime:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(129, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "10";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(531, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Location = new System.Drawing.Point(450, 262);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(250, 47);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(130, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(115, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "next Period";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "previus period";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1159, 561);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Scheduling with a round robin algorithm";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label3;
+        private ListBox listBox3;
+        private Label label2;
+        private ListBox listBox2;
+        private Label label1;
+        private ListBox listBox1;
+        private GroupBox groupBox2;
+        private Button button1;
+        private Label labelSliceTime;
+        private TextBox textBox1;
+        private Label label4;
+        private ListBox listBox4;
+        private CheckBox checkBox1;
+        private TextBox textBox2;
+        private Label label5;
+        private GroupBox groupBox3;
+        private Button button2;
+        private Button button3;
+        private Label label6;
+        private TextBox textBox3;
+        private ListBox listBox5;
+        private Label label7;
+        private TextBox textAvgWaitingTime;
+        private Label label8;
+    }
+}
+
---
+++ OpratingSystemTimeSchedule/OpratingSystemTimeSchedule/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Globalization;
+using System.IO;
+using System.Windows.Forms;
+
+namespace OpratingSystemTimeSchedule
+{
+
+    public partial class Form1 : Form
+    {
+        private int SliceTime { get; set; } = 10;
+        private int DelayTime { get; set; } = 10;
+        private int ContextSwitchTime { get; set; } = 10;
+        
+        public int CurrentTime { get; set; }
+        public int HistoryofCurrentTime { get; set; }
+        public int NewJobCounter;
+        private readonly Queue _processQueue = new Queue();
+        private readonly Queue _doneJob = new Queue();
+        private readonly List<DoneJobLog> _joblog = new List<DoneJobLog>();
+        public int Curstarttext { get; set; }
+
+        public Form1( )
+        {
+            InitializeComponent();            
+        }
+       
+
+        private void ReadJobFromFile() 
+        {
+            using (var reader = new StreamReader(File.OpenRead("Jobs.csv")))
+            {
+                reader.ReadLine();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (line != null)
+                    {
+                        var values = line.Split(',');
+                        Process p1 = new Process(values[0], Convert.ToInt32(values[1]), SliceTime);
+                        _processQueue.Enqueue(p1);
+                        listBox4.Items.Add($"processname = {p1.Name} Runningtime= {p1.Runningtime} RemainTime={p1.Remaintime}");
+                        listBox4.SelectedIndex = listBox4.Items.Count - 1;
+                    }
+                }
+
+            }
+        }
+
+        private void ResetAllObjects()
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            listBox4.Items.Clear();
+            listBox5.Items.Clear();
+            _doneJob.Clear();
+            _joblog.Clear();
+            CurrentTime = 0;
+            HistoryofCurrentTime = 0;
+            groupBox3.Visible = false;
+            textAvgWaitingTime.Text = "";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Timer t1 = new Timer {Interval = 1000};
+            t1.Tick += timer1_Tick;
+            t1.Enabled = true;
+            t1.Start();
+            ResetAllObjects();
+
+            SliceTime = Convert.ToInt32(textBox1.Text);
+            ContextSwitchTime = Convert.ToInt32(textBox2.Text);
+            DelayTime = Convert.ToInt32(textBox3.Text);
+            
+            ReadJobFromFile();
+            
+             
+            while (true)
+            {
+                System.Threading.Thread.Sleep(DelayTime);
+                if (_processQueue.Count == 0)
+                {
+                    break;
+                }
+                
+                var p=(Process) _processQueue.Dequeue();
+                DoneJobLog tmp = new DoneJobLog();
+                tmp.Start = CurrentTime;
+                tmp.Processname = p.Name;
+                var currentTime = CurrentTime;
+                p.DoJob(ref currentTime);
+                CurrentTime = currentTime;
+                tmp.End = currentTime;
+                listBox2.Items.Add(tmp.Processname);
+                listBox2.SelectedIndex = listBox2.Items.Count - 1;
+                _joblog.Add(tmp);
+
+                if (p.Remaintime > SliceTime)
+                {
+                    _processQueue.Enqueue(p);
+                }
+                else
+                {
+                    _doneJob.Enqueue(p);
+                    listBox1.Items.Add(p.Name);
+                    listBox1.SelectedIndex = listBox1.Items.Count - 1;
+                    listBox5.Items.Add(p.GetWaitTime().ToString());
+                }
+                Refresh();
+                CurrentTime+= ContextSwitchTime;
+
+                listBox3.Items.Clear();
+                for (int i = 0; i < _processQueue.Count  ; i++)
+                {
+                    Process a = (Process)_processQueue.Dequeue();
+                    listBox3.Items.Add($"processname = {a.Name} Runningtime= {a.Runningtime} RemainTime={a.Remaintime}");
+                    _processQueue.Enqueue(a);
+                }
+            }
+            t1.Enabled = false;
+            t1.Stop();
+            HistoryofCurrentTime = CurrentTime;
+            groupBox3.Visible = true;
+            ComputeAvrageWaitingTime();
+        }
+
+        private void ComputeAvrageWaitingTime()
+        {
+            long sum=0;
+            for (int i = 0; i < _doneJob.Count; i++)
+            {
+                Process a = (Process)_doneJob.Dequeue();
+                sum += a.GetWaitTime();
+                _doneJob.Enqueue(a);
+            }
+            textAvgWaitingTime.Text = ((decimal)sum / _doneJob.Count).ToString(CultureInfo.InvariantCulture);
+        }
+        private void DrawRotatedTextAt(Graphics gr, float angle,
+            string txt, int x, int y, Font theFont, Brush theBrush)
+        {
+            GraphicsState state = gr.Save();
+            gr.ResetTransform();
+            gr.RotateTransform(angle);
+            gr.TranslateTransform(x, y, MatrixOrder.Append);
+            gr.DrawString(txt, theFont, theBrush, 0, 0);
+            gr.Restore(state);
+        }
+
+
+        public void PaintLogs()
+        { 
+        var graphicsObj = CreateGraphics();
+            Pen myPen = new Pen(Color.Black, 3);
+            Pen clearPen = new Pen(BackColor, 3);
+            int startx1 = 100;
+            int starty1 = 200;
+            int startx2 = 1100;
+            int starty2 = 200;
+
+            int startpoint = -15;
+            
+            graphicsObj.DrawLine(myPen, startx1, starty1, startx2, starty2);
+            var myFont = new Font("Tahoma", 8);
+
+            Brush myBrush = new SolidBrush(Color.Red);
+             
+            int starttext =  (CurrentTime / 100) * 100 ;
+            
+            int endtext = ((CurrentTime / 100)+ 1)  * 100;
+
+            if (Curstarttext != starttext)
+            {
+                graphicsObj.DrawLine(clearPen, startx1, starty1+15, startx2, starty2+15);
+            }
+
+            Curstarttext = starttext;
+
+
+            Pen pen1 = new Pen(Color.Blue, 2);
+            Pen pen2 = new Pen(Color.DeepPink, 2);
+            
+
+            DrawRotatedTextAt(graphicsObj, -90, starttext.ToString(), startx1, starty1 + startpoint, myFont, myBrush);
+            DrawRotatedTextAt(graphicsObj, -90, endtext.ToString(), startx2, starty2 + startpoint, myFont, myBrush);
+
+            int counter = 0;
+            if (_joblog != null)
+                foreach (var a in _joblog)
+                {
+                    if ((a.Start >= starttext && a.Start <= endtext) || (a.End >= starttext && a.End <= endtext))
+                    {
+                        var fStart = a.Start >= starttext ? a.Start : starttext;
+                        var fEnd = a.End <= endtext ? a.End : endtext;
+                        counter++;
+                        Brush aBrush = new SolidBrush(counter % 2 == 0 ? pen1.Color : pen2.Color);
+                        var x1 = ((startx2 - startx1) / 100) * (fStart % 100) + startx1;
+                        DrawRotatedTextAt(graphicsObj, -90, fStart.ToString(), x1, starty1 + startpoint, myFont,
+                            aBrush);
+                        var x2 = ((startx2 - startx1) / 100) * (fEnd % 100) + startx1;
+                        if (x2 < x1) x2 = startx2;
+                        DrawRotatedTextAt(graphicsObj, -90, fEnd.ToString(), x2, starty1 + startpoint, myFont, aBrush);
+                        graphicsObj.DrawLine(counter % 2 == 0 ? pen1 : pen2, x1, starty1 + 5, x1, starty1 - 5);
+                        graphicsObj.DrawLine(counter % 2 == 0 ? pen1 : pen2, x2, starty1 + 5, x2, starty1 - 5);
+
+                        graphicsObj.DrawLine(counter % 2 == 0 ? pen1 : pen2, x1, starty1 - startpoint, x2,
+                            starty1 - startpoint);
+                        DrawRotatedTextAt(graphicsObj, +90, a.Processname,
+                            x1 + (((startx2 - startx1) / 100) * ((fEnd - fStart) / 2)), starty1 + 20, myFont, aBrush);
+                    }
+
+                }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!checkBox1.Checked) return;
+            Random rnd = new Random();
+            int jobduration = rnd.Next(1, 50);
+            NewJobCounter++;
+            Process p1 = new Process($"AP{NewJobCounter}", jobduration, SliceTime);
+            _processQueue.Enqueue(p1);
+            listBox4.Items.Add($"processname = {p1.Name} Runningtime= {p1.Runningtime} RemainTime={p1.Remaintime}");
+            listBox4.SelectedIndex = listBox4.Items.Count - 1;
+        }
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            PaintLogs();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (CurrentTime < 100) return;
+            CurrentTime -= 100;
+            Refresh();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (CurrentTime + 100 > HistoryofCurrentTime) return;
+            CurrentTime += 100;
+            Refresh();
+        }
+    }
+    public class DoneJobLog
+    {
+        public int Start;
+        public int End;
+        public string Processname;
+    }
+    public class ProcessWork
+    {
+        public int Start;
+        public int End;
+    }
+
+    public class Process
+    {
+
+        public Process(string name, int runningtime, int slicetime)
+        {
+            Runningtime = runningtime;
+            Slicetime = slicetime;
+            Remaintime = runningtime;
+            Name = name;
+            ProcessWork = new List<ProcessWork>();
+        }
+
+        public void DoJob(ref int currentTime)
+        {
+            ProcessWork tmp = new ProcessWork {Start = currentTime};
+            currentTime += Remaintime - Slicetime >= 0 ? Slicetime : Remaintime;
+            tmp.End = currentTime;
+            ProcessWork.Add(tmp);
+            Remaintime = Remaintime - Slicetime>=0? Remaintime - Slicetime : Slicetime - Remaintime;
+        }
+
+        public int GetWaitTime()
+        {
+            int previusRun = 0;
+            int result = 0;
+            foreach (var p in ProcessWork)
+            {
+                result += p.Start - previusRun;
+                previusRun = p.End;
+            }
+            return result;
+        }
+        public string Name { get; set; }
+        public int Runningtime { get; set; }
+        public int Remaintime { get; set; }
+        public int Slicetime { get; }
+        public List<ProcessWork> ProcessWork { get; set; }
+    }
+}
